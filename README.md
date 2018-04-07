88d0e3d6ab201de19f9033f7a79fe04508a7ae90
pritam ghosh <19pritamghosh93@gmail.com>
2018-04-07 16:38:04 +0530

    first commit

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.7.3.
 
+run npm install ngx-bootstrap bootstrap --save
+
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import {  Routes, RouterModule } from '@angular/router';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
-
+export const ROUTES: Routes = [
+   { path: '', component: HomeComponent 
+//   children: [
+//     { path: '', component: RestaurantListComponent},
+//     { path: 'restaurant/:id', component: RestaurantDetailComponent}
+//   ]
+ },
+ { path: 'home',redirectTo: '' },
+{ path: 'about', component: AboutComponent }
+    ];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+      FormsModule,
+      ReactiveFormsModule,
+      HttpModule,
+      ModalModule.forRoot(),
+      BsDatepickerModule.forRoot(),
+      BsDropdownModule.forRoot(),
+      RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
