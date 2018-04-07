679dc53abbc51fb7ea6fd0a04bb66ecca521e872
pritam ghosh <19pritamghosh93@gmail.com>
2018-04-08 04:43:39 +0530

    adding home components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,43 +2,52 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import {  Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { EventComponent } from './home/event/event.component';
+import { NewsComponent } from './home/news/news.component';
+import { CarouselComponent } from './home/carousel/carousel.component';
 
 export const ROUTES: Routes = [
-   { path: '', component: HomeComponent 
-//   children: [
-//     { path: '', component: RestaurantListComponent},
-//     { path: 'restaurant/:id', component: RestaurantDetailComponent}
-//   ]
- },
- { path: 'home',redirectTo: '' },
-{ path: 'about', component: AboutComponent }
-    ];
+  {
+    path: '', component: HomeComponent
+    //   children: [
+    //     { path: '', component: RestaurantListComponent},
+    //     { path: 'restaurant/:id', component: RestaurantDetailComponent}
+    //   ]
+  },
+  { path: 'home', redirectTo: '' },
+  { path: 'about', component: AboutComponent }
+];
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    EventComponent,
+    NewsComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
-      FormsModule,
-      ReactiveFormsModule,
-      HttpModule,
-      ModalModule.forRoot(),
-      BsDatepickerModule.forRoot(),
-      BsDropdownModule.forRoot(),
-      RouterModule.forRoot(ROUTES)
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    ModalModule.forRoot(),
+    BsDatepickerModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    RouterModule.forRoot(ROUTES), 
+    CarouselModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/carousel/carousel.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carousel',
+  templateUrl: './carousel.component.html',
+  styleUrls: ['./carousel.component.css']
+})
+export class CarouselComponent implements OnInit {
+  slides: {
+    src: string,
+    captionHeading: string
+    captionBody: string
+  }[]=[];
+  constructor() { }
+
+  ngOnInit() {
+
+    this.slides.push({
+      'src': 'https://valor-software.com/ngx-bootstrap/assets/images/nature/1.jpg',
+      'captionHeading': '',
+      'captionBody': 'test 1'
+    });
+    this.slides.push({
+      'src': 'https://valor-software.com/ngx-bootstrap/assets/images/nature/3.jpg',
+      'captionHeading': '',
+      'captionBody': 'test 2'
+    });
+    this.slides.push({
+      'src': 'https://valor-software.com/ngx-bootstrap/assets/images/nature/2.jpg',
+      'captionHeading': '',
+      'captionBody': 'test 3'
+    });
+    this.slides.push({
+      'src': 'https://valor-software.com/ngx-bootstrap/assets/images/nature/4.jpg',
+      'captionHeading': '',
+      'captionBody': 'test 4'
+    });
+  }
+
+}
