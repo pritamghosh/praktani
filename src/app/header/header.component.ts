00e616abbaca7926d099a6d6d5f69460fe6d80e7
pritam ghosh <19pritamghosh93@gmail.com>
2018-04-17 01:58:16 +0530

    adding account dropdwn

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  collapse: boolean = true;
+  isLoggedIn: boolean = true;
 
   constructor() { }
 
